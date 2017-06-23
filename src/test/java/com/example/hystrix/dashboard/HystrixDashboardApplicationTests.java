27df7692f67025b1978509b8f8b76f8d77764309
eralmas7 <eralmas7@yahoo.com>
2017-06-24 00:07:05 +0530

    Hystrix Dashboard for monitoring

---
+++ src/test/java/com/example/hystrix/dashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.hystrix.dashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.hystrix.dashboard.HystrixDashboardApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = HystrixDashboardApplication.class)
+public class HystrixDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
