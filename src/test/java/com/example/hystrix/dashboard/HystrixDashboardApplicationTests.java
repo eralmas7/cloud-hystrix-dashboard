71f61cea58f0679880e8608d07c6c2c8fca5c8e9
eralmas7 <eralmas7@yahoo.com>
2022-01-01 22:54:00 +0530

    Upgraded spring boot version to 2021.0.0

--- src/test/java/com/example/hystrix/dashboard/HystrixDashboardApplicationTests.java
+++ src/test/java/com/example/hystrix/dashboard/HystrixDashboardApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.hystrix.dashboard;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.hystrix.dashboard.HystrixDashboardApplication;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = HystrixDashboardApplication.class)
 public class HystrixDashboardApplicationTests {
 
