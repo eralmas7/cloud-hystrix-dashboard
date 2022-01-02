c0994f5d64a2c68ece1dd799f822a089e69768a8
eralmas7 <eralmas7@yahoo.com>
2022-01-02 22:28:22 +0530

    Fixed warnings + updated configuration.

--- src/test/java/com/example/hystrix/dashboard/HystrixDashboardApplicationTests.java
+++ src/test/java/com/example/hystrix/dashboard/HystrixDashboardApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.hystrix.dashboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.hystrix.dashboard.HystrixDashboardApplication;
 
