9fe7dad88440ed35ac8b48342a0b5587781c41c4
eralmas7 <eralmas7@yahoo.com>
2022-01-02 23:12:01 +0530

    Removed debug logging.

--- src/test/java/com/example/hystrix/dashboard/HystrixDashboardApplicationTests.java
+++ src/test/java/com/example/hystrix/dashboard/HystrixDashboardApplicationTests.java
@@ -3,8 +3,6 @@ package com.example.hystrix.dashboard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.hystrix.dashboard.HystrixDashboardApplication;
-
 @SpringBootTest(classes = HystrixDashboardApplication.class)
 public class HystrixDashboardApplicationTests {
 
